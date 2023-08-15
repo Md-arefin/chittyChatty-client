cac861bfac07841b33abed66c5a2e7e6c68edb0e
Md-arefin <ansariarefin617@gmail.com>
2023-08-15 22:06:50 +0600

    5 commit

--- src/components/ChatWindow/CharWindow.jsx
+++ src/components/ChatWindow/CharWindow.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const CharWindow = ({ userName }) => {
     return (
         <div>
-            <div className='text-2xl lg:text-7xl text-center Caprasimo mt-10'>Logged in as <span className='text-3xl font-bold'>{userName}</span>
+            <div className='text-xl lg:text-5xl text-center Caprasimo mt-10'>Logged in as <span className='text-2xl lg:text-7xl font-bold'>{userName}</span>
             </div>
         </div>
     );
