5c1d3072dc9361a8d2d0bc90ee3fdda2efc5975b
Md-arefin <ansariarefin617@gmail.com>
2023-08-15 22:54:47 +0600

    5 commit

--- src/components/ChatWindow/CharWindow.jsx
+++ src/components/ChatWindow/CharWindow.jsx
@@ -3,8 +3,8 @@ import img from '../../assets/rsz_11rsz_1rsz_1rsz_11luffy.png'
 
 const CharWindow = ({ userName }) => {
     return (
-        <div>
-            <div className='flex items-center justify-center gap-5 mt-5'>
+        <div className='relative mt-5 border-2 border-white rounded-md mx-10 md:mx-20 lg:mx-40'>
+            <div className='flex items-center sticky-top justify-start gap-5 border-b-[1px] border-white mx-auto'>
                 <div className="avatar">
                     <div className="w-24 rounded-full">
                         <img src={img} />
@@ -13,11 +13,11 @@ const CharWindow = ({ userName }) => {
 
                 <div className='text-xl lg:text-5xl text-center Caprasimo'>Logged in as <span className='text-2xl lg:text-7xl font-bold'>{userName}</span>
                 </div>
-
             </div>
 
 
-            <div>
+            <div className='p-5'>
+               
 
             </div>
 
